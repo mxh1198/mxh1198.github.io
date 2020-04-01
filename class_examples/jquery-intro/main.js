2118af91cf02963e219f1c3dbf9bb1904c1c2a4a
mxh1198 <60359374+mxh1198@users.noreply.github.com>
2020-04-01 17:19:16 +0100

    class on monday

--- class_examples/jquery-intro/main.js
+++ class_examples/jquery-intro/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   //all js goes in here
   console.log("we loaded");
 
-  $("#title").css("color", "#badass");
+  $("#title").css("color", "#bada55");
   $("#title").css("font-size", "125px");
   $("#title").html("<p>More information #1</p>");
 });
