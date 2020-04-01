3b7d8d105b26329385207371fc0ae56b33098663
mxh1198 <60359374+mxh1198@users.noreply.github.com>
2020-04-01 18:36:09 +0100

    class ended wednesday

--- class_examples/jquery-intro/main.js
+++ class_examples/jquery-intro/main.js
@@ -2,7 +2,27 @@ $(document).ready(function() {
   //all js goes in here
   console.log("we loaded");
 
-  $("#title").css("color", "#bada55");
-  $("#title").css("font-size", "125px");
-  $("#title").html("<p>More information #1</p>");
+  $("#button1").click(function() {
+    // code that runs when you click on the part of the page that has the id="title"
+    // $("#title").css("font-size", "125px");
+    // $("#title").css("color", "#f9423a");
+    $("#title").addClass("big-text");
+  });
+
+  $("#button2").click(function() {
+    // stuff we put here will happen when you click button 2.
+    $("#title").removeClass("big-text");
+  });
+
+  $("p").mouseover(function() {
+    $("p").css("font-size", "21px");
+  });
+
+  $("p").mouseleave(function() {
+    $("p").css("background-color", "yellow");
+    $("p").css("font-size", "16px");
+  });
+  // $("#title").css("color", "#bada55");
+  // $("#title").css("font-size", "125px");
+  // $("#title").html("<p>More information #1</p>");
 });
