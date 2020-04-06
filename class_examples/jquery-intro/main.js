8b7b94fd9e5ddc2a4b99183b5e6e7ac6718ca226
mxh1198 <60359374+mxh1198@users.noreply.github.com>
2020-04-06 19:02:05 +0100

    monday class

--- class_examples/jquery-intro/main.js
+++ class_examples/jquery-intro/main.js
@@ -1,28 +1,31 @@
-$(document).ready(function() {
-  //all js goes in here
-  console.log("we loaded");
+$(document)
+  .ready(function() {
+    //all js goes in here
+    console.log("we loaded");
 
-  $("#button1").click(function() {
-    // code that runs when you click on the part of the page that has the id="title"
-    // $("#title").css("font-size", "125px");
-    // $("#title").css("color", "#f9423a");
-    $("#title").addClass("big-text");
-  });
+    $("#button1").click(function() {
+      // code that runs when you click on the part of the page that has the id="title"
+      // $("#title").css("font-size", "125px");
+      // $("#title").css("color", "#f9423a");
+      $("#title").addClass("big-text");
+      console.log("this ran");
+    });
 
-  $("#button2").click(function() {
-    // stuff we put here will happen when you click button 2.
-    $("#title").removeClass("big-text");
-  });
+    $("#button2").click(function() {
+      // stuff we put here will happen when you click button 2.
+      $("#title").removeClass("big-text");
+    });
 
-  $("p").mouseover(function() {
-    $("p").css("font-size", "21px");
-  });
+    $("p").mouseover(function() {
+      $("p").css("font-size", "21px");
+    });
 
-  $("p").mouseleave(function() {
-    $("p").css("background-color", "yellow");
-    $("p").css("font-size", "16px");
+    $("p").mouseleave(function() {
+      $("p")
+        .css("background-color", "yellow");
+      $("p").css("font-size", "16px");
+    });
+    $("#box").mousedown(function() {
+      $("#box").css("background-color", "green");
+    });
   });
-  // $("#title").css("color", "#bada55");
-  // $("#title").css("font-size", "125px");
-  // $("#title").html("<p>More information #1</p>");
-});
